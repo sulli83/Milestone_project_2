c3eddcf3a4a5d68624c97e2d15a3fc094c161a7c
Shaun O'Sullivan <shaunosullivan83@gmail.com>
2018-11-25 21:20:34 +0000

    update on social housing piecharts

--- static/js/graph1.js
+++ static/js/graph1.js
@@ -1,14 +1,14 @@
   queue()
-            .defer(d3.csv, "/housing data/Social Housing Construction Status Report Q1 2018.csv")
+            .defer(d3.csv, "/housing data/Social Housing Construction Status Report Q1 2017.csv")
             .await(makeGraphs);
         
         function makeGraphs(error, housingData) {
             var ndx = crossfilter(housingData);
            
            
-            var name_dim = ndx.dimension(dc.pluck('Local Authority'));
+            var name_dim = ndx.dimension(dc.pluck('LA'));
             var total_number_of_units = name_dim.group().reduceSum(dc.pluck('No. of Units'));
-            dc.pieChart('#units_per_LA')
+            dc.pieChart('#units_per_LA_2')
                 .height(1000)
                 .radius(400)
                 .transitionDuration(1500)
@@ -18,7 +18,7 @@
             
             var FP_dim = ndx.dimension(dc.pluck('Funding Programme'));
             var total_units_per_FP = FP_dim.group().reduceSum(dc.pluck('No. of Units'));
-            dc.pieChart('#per-store-chart')
+            dc.pieChart('#per-store-chart_2')
                 .height(330)
                 .radius(400)
                 .transitionDuration(1500)
@@ -37,11 +37,3 @@
             dc.renderAll();
         }
         
-      
-
-
-
-
-    
-      
-
